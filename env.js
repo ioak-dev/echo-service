883c992505136ae4107ed9145c0c30034bde8edb
Arun Kumar Selvaraj <arun@ioak.io>
2025-05-16 15:25:08 +0530

    Delete env.js

--- env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    CHATGPT_API_KEY:
-        process.env.CHATGPT_API_KEY ||
-        "sk-proj-ypujYZvr1RDkmp3ZlUmtT3BlbkFJdRbWD9cvaKJiXbZ3OZ6Z",
-    GEMINI_API_KEY:
-        process.env.GEMINI_API_KEY || "AIzaSyAq1gQekrycyU0wp5n9IFIMKMZktO_rX68",
-};
