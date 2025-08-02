2a3929b9e09eddf93da1ffc7192f08dcf0e34c52
Arun S <arun@ioak.io>
2025-08-03 01:11:25 +0530

    Simplified implementation of RAG stage 1 to consolidate data based on entity relation spec

--- src/modules/domain/api/rag copy/utils.ts
+++
@@ -1,7 +0,0 @@
-import crypto from 'crypto';
-
-export const computeHash = (text: string): string =>
-    crypto.createHash('sha256').update(text).digest('hex');
-
-export const formatFieldNameAsLabel = (field: string): string =>
-    field.replace(/[_\-]/g, ' ').replace(/\b\w/g, (char) => char.toUpperCase());
